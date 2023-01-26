29a45a1646cc9d119b4d8d2dad8383b93fe2d0f7
Nikita Tolstik <ntolstik@aristeksystems.com>
2023-01-26 21:59:14 +0300

    feat: second commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { UserService } from '../user/user.service';
+import { AuthService } from './auth.service';
+
+@Module({
+  providers: [AuthService, UserService],
+})
+export class AuthModule {}
